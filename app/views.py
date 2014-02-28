7e7959e060da6c0b97d917f46a40bc70e001e1c8
Roman Grankin <grankin.roman@hotmail.com>
2014-02-28 19:11:56 +0200

    re

---
+++ app/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template, flash, redirect, request
+from forms import LoginForm
+
+#----------------------------------------
+# controllers
+#----------------------------------------
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, '/static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+
